b87ffb0636af2d3bd8aa9151e3801525e0d068d1
SoftLife0 <onikosiadewale18@gmail.com>
2024-03-15 21:11:11 +0000

    adding more features

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../components/Header';
-import { BiReceipt } from 'react-icons/bi';
+import { BiReceipt, BiCoin } from 'react-icons/bi';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 
@@ -34,7 +34,7 @@ function Dashboard() {
                           <div data-aos="fade-up" data-aos-delay="100" className="card" style={{background:'#f1f6ff'}}>
                               <h6>Sender Id</h6>
                               <div className='d-flex justify-content-between align-items-center'>
-                                <BiReceipt style={{ fontSize: "25px" }} />
+                                <BiCoin style={{ fontSize: "25px" }} />
                                 <h2 style={{ fontWeight: 700 }}>0</h2>
                             </div>
                           </div>
