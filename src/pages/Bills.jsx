1601fabc4718eee397a36391d804d883f16215b3
SoftLife0 <onikosiadewale18@gmail.com>
2024-03-16 19:04:06 +0000

    still cooking this shit

--- src/pages/Bills.jsx
+++ src/pages/Bills.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BiReceipt } from 'react-icons/bi';
+import { Receipt} from 'react-bootstrap-icons';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 
@@ -9,13 +9,120 @@ function Bills() {
         <div className="row">
             <div className="col-md-6">
                 <Link to="#" style={{ textDecoration: 'none', color: 'black'}}>
-                <div data-aos="fade-up" className="card" style={{background:'#f1f6ff'}}>
-                    <h6>Income</h6>
-                    <div className='d-flex justify-content-between align-items-center'>
-                        <BiReceipt style={{ fontSize: "25px" }} />
-                        <h2 style={{ fontWeight: 700 }}>0</h2>
+                    <div data-aos="fade-up" className="card" style={{padding:'10px 30px'}}>
+                        <div className='d-flex justify-content-between align-items-center'>
+                            <div className='d-flex justify-content-between align-items-center'>
+                                <div className="box expense" style={{margin:'0'}}></div>
+                                <p className='text-muted mb-0' style={{marginLeft:'3px', fontSize:'13px'}}>Bill</p>
+                            </div>
+                            <Receipt style={{ fontSize: "15px" }} />
+                        </div>
+                        <div className='mt-2'>
+                            <h6>Iklina</h6>
+
+                            <div className="d-flex justify-content-between">
+                                <small className="text-muted">₵0</small>
+                                <small className="text-muted">₵867</small>
+                            </div>
+                            <div className="progress" style={{height: '5px'}}>
+                                <div className="progress-bar" role="progressbar" style={{width: '30%'}} aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                            <div className="d-flex justify-content-between">
+                                <small className="text-muted">30%</small>
+                                <small className="text-muted">₵256 more to go</small>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                </Link>
+            </div>
+
+
+            <div className="col-md-6">
+                <Link to="#" style={{ textDecoration: 'none', color: 'black'}}>
+                    <div data-aos="fade-up" className="card" style={{padding:'15px 30px'}}>
+                        <div className='d-flex justify-content-between align-items-center'>
+                            <div className='d-flex justify-content-between align-items-center'>
+                                <div className="box expense" style={{margin:'0'}}></div>
+                                <p className='text-muted mb-0' style={{marginLeft:'3px', fontSize:'13px'}}>Bill</p>
+                            </div>
+                            <Receipt style={{ fontSize: "15px" }} />
+                        </div>
+                        <div className='mt-2'>
+                            <h6>AWS Server Bill</h6>
+
+                            <div className="d-flex justify-content-between">
+                                <small className="text-muted">₵0</small>
+                                <small className="text-muted">₵300</small>
+                            </div>
+                            <div className="progress" style={{height: '5px'}}>
+                                <div className="progress-bar" role="progressbar" style={{width: '0%'}} aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                            <div className="d-flex justify-content-between">
+                                <small className="text-muted">0%</small>
+                                <small className="text-muted">₵300 more to go</small>
+                            </div>
+                        </div>
+                    </div>
+                </Link>
+            </div>
+
+
+            <div className="col-md-6">
+                <Link to="#" style={{ textDecoration: 'none', color: 'black'}}>
+                    <div data-aos="fade-up" className="card" style={{padding:'10px 30px'}}>
+                        <div className='d-flex justify-content-between align-items-center'>
+                            <div className='d-flex justify-content-between align-items-center'>
+                                <div className="box expense" style={{margin:'0'}}></div>
+                                <p className='text-muted mb-0' style={{marginLeft:'3px', fontSize:'13px'}}>Bill</p>
+                            </div>
+                            <Receipt style={{ fontSize: "15px" }} />
+                        </div>
+                        <div className='mt-2'>
+                            <h6>Transportation</h6>
+
+                            <div className="d-flex justify-content-between">
+                                <small className="text-muted">₵0</small>
+                                <small className="text-muted">₵500</small>
+                            </div>
+                            <div className="progress" style={{height: '5px'}}>
+                                <div className="progress-bar" role="progressbar" style={{width: '70%'}} aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                            <div className="d-flex justify-content-between">
+                                <small className="text-muted">70%</small>
+                                <small className="text-muted">₵135 more to go</small>
+                            </div>
+                        </div>
+                    </div>
+                </Link>
+            </div>
+
+
+            <div className="col-md-6">
+                <Link to="#" style={{ textDecoration: 'none', color: 'black'}}>
+                    <div data-aos="fade-up" className="card" style={{padding:'10px 30px'}}>
+                        <div className='d-flex justify-content-between align-items-center'>
+                            <div className='d-flex justify-content-between align-items-center'>
+                                <div className="box expense" style={{margin:'0'}}></div>
+                                <p className='text-muted mb-0' style={{marginLeft:'3px', fontSize:'13px'}}>Bill</p>
+                            </div>
+                            <Receipt style={{ fontSize: "15px" }} />
+                        </div>
+                        <div className='mt-2'>
+                            <h6>Others</h6>
+
+                            <div className="d-flex justify-content-between">
+                                <small className="text-muted">₵0</small>
+                                <small className="text-muted">₵8000</small>
+                            </div>
+                            <div className="progress" style={{height: '5px'}}>
+                                <div className="progress-bar" role="progressbar" style={{width: '50%'}} aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                            <div className="d-flex justify-content-between">
+                                <small className="text-muted">50%</small>
+                                <small className="text-muted">₵4000 more to go</small>
+                            </div>
+                        </div>
+                    </div>
                 </Link>
             </div>
         </div>    
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from '../components/Header';
+import Chart from './Chart';
 import { BiReceipt, BiCoin } from 'react-icons/bi';
 import { CashCoin, PiggyBank, GraphUpArrow, Receipt} from 'react-bootstrap-icons';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
@@ -21,11 +22,11 @@ function Dashboard() {
                   <div className="row">
                       <div className="col-md-6">
                       <Link to="#" style={{ textDecoration: 'none', color: 'black'}}>
-                        <div data-aos="fade-up" className="card" style={{background:'#f1f6ff'}}>
-                            <h6>Income</h6>
+                        <div data-aos="fade-up" className="card">
+                            <h6>Deposit</h6>
                             <div className='d-flex justify-content-between align-items-center'>
-                                <BiReceipt style={{ fontSize: "25px" }} />
-                                <h2 style={{ fontWeight: 700 }}>0</h2>
+                                <PiggyBank style={{ fontSize: "25px" }} />
+                                <h2 style={{ fontWeight: 700 }}>₵1200</h2>
                             </div>
                         </div>
                       </Link>
@@ -33,11 +34,11 @@ function Dashboard() {
 
                       <div className="col-md-3 col-6">
                         <Link to="#" style={{ textDecoration: 'none', color: 'black'}}>
-                          <div data-aos="fade-up" data-aos-delay="100" className="card" style={{background:'#f1f6ff'}}>
+                          <div data-aos="fade-up" data-aos-delay="100" className="card">
                               <h6>Bills</h6>
                               <div className='d-flex justify-content-between align-items-center'>
                                 <Receipt style={{ fontSize: "25px" }} />
-                                <h2 style={{ fontWeight: 700 }}>0</h2>
+                                <h2 style={{ fontWeight: 700 }}>8</h2>
                             </div>
                           </div>
                         </Link>
@@ -45,11 +46,11 @@ function Dashboard() {
 
                       <div className="col-md-3 col-6">
                         <Link to="#" style={{ textDecoration: 'none', color: 'black'}}>
-                          <div data-aos="fade-up" data-aos-delay="100" className="card" style={{background:'#f1f6ff'}}>
+                          <div data-aos="fade-up" data-aos-delay="100" className="card">
                               <h6>Transactions</h6>
                               <div className='d-flex justify-content-between align-items-center'>
                                 <CashCoin style={{ fontSize: "25px" }} />
-                                <h2 style={{ fontWeight: 700 }}>0</h2>
+                                <h2 style={{ fontWeight: 700 }}>21</h2>
                             </div>
                           </div>
                         </Link>
@@ -100,47 +101,7 @@ function Dashboard() {
 
 
               <div className="col-md-5">
-                <div className="card" style={{background:'#f1f6ff'}}>
-                    <div className="card-body d-flex flex-column justify-content-center align-items-center">
-                        <h5 className="card-title">Net Worth</h5>
-                        {/* Circle with text */}
-                        <div className="circle-container">
-                            <div className="circle">
-                                <div className="circle-content">$500,000</div>
-                            </div>
-                        </div>
-
-                        <div className="details">
-                            <ul className="list-unstyled">
-                                <li>
-                                    <div className="asset-expense-other d-flex justify-content-between align-items-center">
-                                      <div className='d-flex justify-content-between align-items-center'>
-                                        <span>Asset</span>
-                                        <div className="box asset"></div>
-                                      </div>
-                                        <span>$150000</span>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div className="asset-expense-other d-flex justify-content-between align-items-center">
-                                        <span>Expense</span>
-                                        <div className="box expense mr-5"></div>
-                                        <span>$50000</span>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div className="asset-expense-other d-flex justify-content-between align-items-center">
-                                        <span>Other</span>
-                                        <div className="box other mr-2"></div>
-                                        <span>$10000</span>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                        {/* Button to view details */}
-                        <button type="button" className="pill-button">View Details</button>
-                    </div>
-                </div>
+                <Chart />
               </div>
 
           </div>
