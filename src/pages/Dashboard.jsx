f07ce0efa4c2bc97b3a3a7b8d6c362ff18a6f17d
SoftLife0 <onikosiadewale18@gmail.com>
2024-03-18 19:31:48 +0000

    working on build

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -57,17 +57,7 @@ function Dashboard() {
                       </div>
 
                       {/* 
-                      <div className="col-md-6 col-6">
-                        <Link to="#" style={{ textDecoration: 'none', color: 'black'}}>
-                          <div data-aos="fade-up" data-aos-delay="100" className="card" style={{background:'#f1f6ff'}}>
-                              <h6>Report</h6>
-                              <div className='d-flex justify-content-between align-items-center'>
-                                <GraphUpArrow style={{ fontSize: "25px" }} />
-                                <h2 style={{ fontWeight: 700 }}>0</h2>
-                            </div>
-                          </div>
-                        </Link>
-                      </div>
+                      
 
                       <div className="col-md-6 col-6">
                         <Link to="#" style={{ textDecoration: 'none', color: 'black'}}>
